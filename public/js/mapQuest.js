b4e99f125002de9097d04941c40af586a2c9c700
tiffany-meng <tmeng@ucsd.edu>
2021-02-25 12:17:24 -0800

    lab8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
-	// add your code here
+  L.mapquest.key = 'Z1YXIPGGDL6iMB5ksMKEqMTJypJ5pXQQ';
+
+  // 'map' refers to a <div> element with the ID map
+  var map = L.mapquest.map('map', {
+    center: [32.87863177066982, -117.23579972871384],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 12
+  });
+
+  L.marker([32.87863177066982, -117.23579972871384]).addTo(map);
 }
